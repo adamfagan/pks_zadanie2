bd5a014c6fa7849b1f7a146030a5dc386a644534
Adam Fagan <adam.fagann@gmail.com>
2020-11-27 00:02:25 +0100

    Nova verzia

--- main.py
+++ main.py
@@ -6,9 +6,8 @@ import socket
 import os
 
 # ACK = 01
-# SYN = 02
-# INF = 03
-# SYN + ACK = 21
+# INF = 02
+
 
 def parse_data(data):
     parsed_data = {'flg': int(data.hex()[6:8], 16), 'sqn': int(data.hex()[14:16], 16)}
@@ -28,58 +27,46 @@ def parse_data(data):
     parsed_data['fnm'] = bytes.fromhex(file_name).decode("ASCII")
     return parsed_data
 
-def create_header(flag, sequence_num, count_fragments, file_name):
-    if flag == "SYN":
-        flg = bytes('flg', 'utf-8') + bytes([2])
-        sqn = bytes('sqn', 'utf-8') + bytes([sequence_num])
-        header = flg + sqn
-        return header
-    elif flag == "SYN+ACK":
-        flg = bytes('flg', 'utf-8') + bytes([21])
-        sqn = bytes('sqn', 'utf-8') + bytes([sequence_num])
-        header = flg + sqn
-        return header
-    elif flag == "ACK":
-        flg = bytes('flg', 'utf-8') + bytes([1])
-        sqn = bytes('sqn', 'utf-8') + bytes([sequence_num])
-        header = flg + sqn
-        return header
-    elif flag == "INF":
-        flg = bytes('flg', 'utf-8') + bytes([3])
-        sqn = bytes('sqn', 'utf-8') + bytes([sequence_num])
-        fragments = []
-        while count_fragments > 255:
-            fragments.append(int(255))
-            count_fragments -= 255
-        fragments.append(count_fragments)
-        frg = bytes('frg', 'utf-8') + bytes(fragments)
-        fnm = bytes('fnm', 'utf-8') + bytes(file_name, 'utf-8')
-        header = flg + sqn + frg + fnm
-        return header
+def create_header(flag, sequence_num, count_fragments, fragment_size):
+    # FLAG(1B) SEQUENCE(3B) COUNT_OF_FRAGMENTS(3B) FRAGMENT_SIZE(2B)
+    #  2            0                 7                   1
 
-def count_of_fragments(file_name):
-    size_of_file = os.path.getsize(file_name)
-    if size_of_file / fragment_size > int(size_of_file / fragment_size):
-        return int((size_of_file / fragment_size)) + 1
-    else:
-        return size_of_file / fragment_size
+    flg = flag.to_bytes(1, 'big')
+    sqn = sequence_num.to_bytes(3, 'big')
+    cof = count_fragments.to_bytes(3, 'big')
+    fgs = fragment_size.to_bytes(2, 'big')
+    return flg + sqn + cof + fgs
 
-def handshake(sock, server_ip, server_port):
-    header = create_header("SYN", 1, 0, 0)
-    sock.sendto(header, (server_ip, server_port))
-    data, server_ip_port = sock.recvfrom(fragment_size)
-    if data.hex()[6:8] == "15":  # SYN + ACK
-        header = create_header("ACK", 3, 0, 0)
-        sock.sendto(header, server_ip_port)
 
-def client_sends_file(server_ip, server_port):
-    client_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    file_name = "test1.txt"  # input("Zadajte nazov suboru aj s typom suboru (nazov.typ): ")
-    count_fragments = count_of_fragments(file_name)  # zistenie poctu posielanych fragmentov
-    handshake(client_sock, server_ip, server_port)  # HANDSHAKE
-    header = create_header("INF", 4, count_fragments, file_name)
-    client_sock.sendto(header, (server_ip, server_port))  # KLIENT posiela INF
+def calculate_fragments(type_of_input, fragment_size, file_or_msg):
+    if type_of_input == 1:  # SPRAVA
+        count_of_fragments = len(file_or_msg) / fragment_size
+        if count_of_fragments > int(count_of_fragments):
+            return int(count_of_fragments) + 1
+        else:
+            return int(count_of_fragments)
+    elif type_of_input == 2:  # SUBOR
+        size_of_file = os.path.getsize(file_or_msg)
+        if size_of_file / fragment_size > int(size_of_file / fragment_size):
+            return int(size_of_file / fragment_size) + 1
+        else:
+            return int(size_of_file / fragment_size)
+
+def client_sends_msg(fragment_size, server_ip, server_port, type, client_sock, message):
+    count_of_fragments = calculate_fragments(type, fragment_size, message)  # zistenie poctu posielanych fragmentov
+
+
+def client_sends_file(fragment_size, server_ip, server_port, type, client_sock, file_name):
+    count_of_name_fragments = calculate_fragments(1, fragment_size, file_name)  # type = 1 -> fragmentuje ako string
+    count_of_file_fragments = calculate_fragments(type, fragment_size, file_name)  # zistenie poctu posielanych fragmentov
+
+    header = create_header(2, 0, count_of_name_fragments, fragment_size)  # INF = 2, SQN = 0
+    inf_message_file_name = header + (2).to_bytes(1, 'big')  # HEADER + DATA
+    client_sock.sendto(inf_message_file_name, (server_ip, server_port))  # KLIENT posiela informacie (INF) SERVERU o pocte fragmentoch s nazvom suboru
+    # SKONCIL SOM VYTVORENIM METODY CREATE_HEADER A ODOSLANIM INF FRAGMENTO O MENE SUBORU, POKRACOVAT S
+    # PARSOVANIM PRIJATEHO FRAGMENTU NA STRANE SERVERA A ODPOVEDANIM SERVERA
     client_sock.recvfrom(fragment_size)  # dostavam ACK na INF
+
     with open(file_name, "rb") as file:  # POSIELANIE DAT
         while True:
             byte = file.read(fragment_size)
@@ -90,54 +77,57 @@ def client_sends_file(server_ip, server_port):
             data_from_server, server_ip_port = client_sock.recvfrom(fragment_size)
             print(data_from_server)
 
+def client():
+    fragment_size = 1         # int(input("Zadajte velkost fragmentu v Bytoch: "))
+    server_ip = "localhost"     # input("Zadajte cielovu IP adresu: ")
+    server_port = 5005          # int(input("Zadajte cielovy port: "))
+    print("1 -> Sprava")
+    print("2 -> Subor")
+    type = 2                    # int(input("Zadajte svoj vyber: "))
+
+    client_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    if type == 1:  # SPRAVA
+        message = "Hello PKS ! Hello PKS ! Hello PKS ! Hello PKS ! Hello PKS ! Hello PKS !"  # input
+        client_sends_msg(fragment_size, server_ip, server_port, type, client_sock, message)
+    elif type == 2:  # SUBOR
+        file_name = "test1.txt"     # input("Zadajte nazov suboru aj s typom suboru (nazov.typ): ")
+        client_sends_file(fragment_size, server_ip, server_port, type, client_sock, file_name)
+
+
+def server():
+    fragment_size = 1         # int(input("Zadajte velkost fragmentu v Bytoch: "))
+    server_ip = "localhost"     # input("Zadajte IP adresu: ")
+    server_port = 5005          # int(input("Zadajte port: "))
+
+    server_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_sock.bind((server_ip, server_port))
+    data, client_ip_port = server_sock.recvfrom(fragment_size)  # SERVER ocakava INF
+    print("prijate")
+    parsed_data = parse_data(data)
+    print(parsed_data)
+    header = create_header("ACK", parsed_data["sqn"] + 1, 0, 0)  # 5 odstranit a nahradit uz parsnutimi datami
+    server_sock.sendto(header, client_ip_port)
+    while True:
+        data, client_ip_port = server_sock.recvfrom(fragment_size)
+        server_sock.sendto("Prijate".encode(), client_ip_port)
 
 print("1 -> Klient")
 print("2 -> Server")
 option = int(input("Zadajte svoju ulohu: "))  # 1 alebo 2
-fragment_size = 200  # int(input("Zadajte velkost fragmentu v Bytoch: "))
-
 if option == 1:  # KLIENT
-    server_ip = "localhost"  # input("Zadajte cielovu IP adresu: ")
-    server_port = 5005  # int(input("Zadajte cielovy port: "))
-    print("1 -> Sprava")
-    print("2 -> Subor")
-    option2 = 2  # int(input("Zadajte svoj vyber: "))
-    if option2 == 2:  # KLIENT posiela SUBOR
-        client_sends_file(server_ip, server_port)
-    elif option2 == 1:  # KLIENT posiela SPRAVU
-        message = input("Napiste spravu: ")
-
+    client()
 elif option == 2:  # SERVER
-    server_ip = "localhost"  # input("Zadajte IP adresu: ")
-    server_port = 5005  # int(input("Zadajte port: "))
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind((server_ip, server_port))
-    data, client_ip_port = sock.recvfrom(fragment_size)  # Tu SERVER prijima SYN
-    if data.hex()[6:8] == "02":  # SYN
-        header = create_header("SYN+ACK", 2, 0, 0)
-        sock.sendto(header, client_ip_port)
-        data, client_ip_port = sock.recvfrom(fragment_size)  # SERVER ocakava ACK
-        if data.hex()[6:8] == "01":  # Prijimam ACK od KLIENTA
-            data, client_ip_port = sock.recvfrom(fragment_size)  # SERVER ocakava INF
-            # Tu v data dostavam pocet fragmentov a meno suboru
-            parsed_data = parse_data(data)
-            print(parsed_data)
-            # TU SOM SKONCIL, V PARSNUTYCH DATACH MAM POCET FRAGMENTOV (SEQUENCE), NA ZAKLADE TOHO
-            # MUSIM ROBIT HLAVICKY DATOVYCH FRAGMENTOV, TAKZE MUSIM ROBIT V HLAVICKE V "SQN" POLE
-            # PRETOZE MAM 270 FRAGMENTOV A TIE MUSIM DAT PO 255 DO POLA
-            # TAKZE PODLA TOHO TREBA ROBIT HLAVICKY
-            header = create_header("ACK", parsed_data["sqn"]+1, 0, 0)  # 5 odstranit a nahradit uz parsnutimi datami
-            sock.sendto(header, client_ip_port)
-            while True:
-                data, client_ip_port = sock.recvfrom(fragment_size)
-                sock.sendto("Prijate".encode(), client_ip_port)
-    else:
-       sock.sendto("Prijate".encode(), client_ip_port)
+    server()
+
 
 # b'\x02\x01'
 # print(bytes.fromhex(data_hex).decode('utf-8'))
 
-
+# TU SOM SKONCIL, V PARSNUTYCH DATACH MAM POCET FRAGMENTOV (SEQUENCE), NA ZAKLADE TOHO
+            # MUSIM ROBIT HLAVICKY DATOVYCH FRAGMENTOV, TAKZE MUSIM ROBIT V HLAVICKE V "SQN" POLE
+            # PRETOZE MAM 270 FRAGMENTOV A TIE MUSIM DAT PO 255 DO POLA
+            # TAKZE PODLA TOHO TREBA ROBIT HLAVICKY
 
 
 """size_of_sqn = count_fragments / 255  # urcovanie poctu bytov v hlavicke pre Sequence  2
